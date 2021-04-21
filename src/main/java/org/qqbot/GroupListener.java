57f48e0165bc65b9bbc0fd404fc637be7f075142
diyigemt <qwe1355247243@gmail.com>
2021-04-21 23:54:36 +0800

    更新缩进为2tab 更新 .gitignore

--- src/main/java/org/qqbot/GroupListener.java
+++ src/main/java/org/qqbot/GroupListener.java
@@ -5,18 +5,18 @@ import net.mamoe.mirai.event.events.GroupMessageEvent;
 import java.util.function.Consumer;
 
 public class GroupListener implements Consumer<GroupMessageEvent> {
-    @Override
-    public void accept(GroupMessageEvent groupMessageEvent) {
-        String content = groupMessageEvent.getMessage().serializeToMiraiCode();
-        if(content.contains("[mirai:at:2492921801]")){
-            switch (groupMessageEvent.getMessage().contentToString()){
-                case "@B73-NS" + "/掷骰子":
-                    break;
-                case "@B73-NS" + "/咩":
-                    break;
-                case "@B73-NS" + "/帮助":
-                    break;
-            }
-        }
+  @Override
+  public void accept(GroupMessageEvent groupMessageEvent) {
+    String content = groupMessageEvent.getMessage().serializeToMiraiCode();
+    if(content.contains("[mirai:at:2492921801]")){
+      switch (groupMessageEvent.getMessage().contentToString()){
+        case "@B73-NS" + "/掷骰子":
+          break;
+        case "@B73-NS" + "/咩":
+          break;
+        case "@B73-NS" + "/帮助":
+          break;
+      }
     }
+  }
 }
--- src/main/java/org/qqbot/Plugin.java
+++ src/main/java/org/qqbot/Plugin.java
@@ -7,19 +7,19 @@ import net.mamoe.mirai.event.Listener;
 import net.mamoe.mirai.event.events.GroupMessageEvent;
 
 public final class Plugin extends JavaPlugin {
-    public static final Plugin INSTANCE = new Plugin();
+  public static final Plugin INSTANCE = new Plugin();
 
-    private Plugin() {
-        super(new JvmPluginDescriptionBuilder("org.qqbot.plugin", "1.0-SNAPSHOT")
-                .name("QQBot")
-                .info("Mirai robot")
-                .author("diyigemt, Haythem")
-                .build());
-    }
+  private Plugin() {
+    super(new JvmPluginDescriptionBuilder("org.qqbot.plugin", "1.0-SNAPSHOT")
+        .name("QQBot")
+        .info("Mirai robot")
+        .author("diyigemt, Haythem")
+        .build());
+  }
 
-    @Override
-    public void onEnable() {
-        getLogger().info("QQ robot Plugin loaded!");
-        Listener listener = GlobalEventChannel.INSTANCE.subscribeAlways(GroupMessageEvent.class, new GroupListener());
-    }
+  @Override
+  public void onEnable() {
+    getLogger().info("QQ robot Plugin loaded!");
+    Listener listener = GlobalEventChannel.INSTANCE.subscribeAlways(GroupMessageEvent.class, new GroupListener());
+  }
 }
