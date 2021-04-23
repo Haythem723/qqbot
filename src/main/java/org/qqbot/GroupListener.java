5c35f9c158ab5d2c01ea77a50e967e4370e39089
diyigemt <qwe1355247243@gmail.com>
2021-04-23 11:02:47 +0800

    no message

---
+++ src/main/java/org/qqbot/GroupListener.java
@@ -0,0 +1,22 @@
+package org.qqbot;
+
+import net.mamoe.mirai.event.events.GroupMessageEvent;
+
+import java.util.function.Consumer;
+
+public class GroupListener implements Consumer<GroupMessageEvent> {
+  @Override
+  public void accept(GroupMessageEvent groupMessageEvent) {
+    String content = groupMessageEvent.getMessage().serializeToMiraiCode();
+    if(content.contains("[mirai:at:2492921801]")){
+      switch (groupMessageEvent.getMessage().contentToString()){
+        case "@B73-NS" + "/掷骰子":
+          break;
+        case "@B73-NS" + "/咩":
+          break;
+        case "@B73-NS" + "/帮助":
+          break;
+      }
+    }
+  }
+}
